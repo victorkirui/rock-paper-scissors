dcfd673f2917027d70c6e85ff925ee7f5d741bbe
victorkirui <vkkiprop@gmail.com>
2024-06-10 21:50:48 +0300

    Refactor getComputerChoice and getHumanChoice functions

--- main.js
+++ main.js
@@ -4,20 +4,42 @@ let computerScore = 0;
 //Get computer input
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3) + 1;
-  return randomChoice;
+  let result;
+  switch (randomChoice) {
+    case 1:
+      result = "Rock";
+      break;
+    case 2:
+      result = "paper";
+      break;
+    case 3:
+      result = "Scissors";
+      break;
+  }
+  return result;
 }
+
+console.log(getComputerChoice());
 
 //Get user input
 function getHumanChoice() {
-  let userInput = window.prompt("Enter any numbers between 1, 2 and 3:");
-
-  if (userInput == 1) {
-    return 1;
-  } else if (userInput == 2) {
-    return 2;
-  } else if (userInput == 3) {
-    return 3;
+  let userInput = window.prompt("Enter 'Rock', 'Paper' or 'Scissors':");
+  //   let result;
+  if (userInput.toLowerCase() == "rock") {
+    // result = "Rock";
+    return "Rock";
+  } else if (userInput.toLowerCase() == "paper") {
+    // result = "Paper";
+    return "Paper";
+  } else if (userInput.toLowerCase() == "scissors") {
+    // result = "Scissors";
+    return "Scissors";
   } else {
     return "Invalid choice - choose numbers 1, 2 or 3";
   }
 }
+
+console.log(getHumanChoice());
+
+//Logic to play a single round
+function playRound(humanChoice, computerChoice) {}
